1916d104d4b081e549f96fa51686351699cb9bb1
engrsyedzain <113347856+engrsyedzain@users.noreply.github.com>
2024-11-04 13:33:45 +0500

    Update apiKey.js

--- app/services/apiKey.js
+++ app/services/apiKey.js
@@ -1,2 +1,2 @@
-export const apiKey = "AIzaSyBSsMRi9tA-lRNnDw9-SD5wfKvds_XvssU";
-export const vimeoApi = "39a4f39906338ca431d4dd81f950f245";
+export const apiKey = "enter-key";
+export const vimeoApi = "enter-key";
